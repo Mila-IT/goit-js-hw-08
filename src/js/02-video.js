7b5ce6a7e88e071a96f6c86831aae0afe3195150
Mila-IT <l.shapkina84@gmail.com>
2023-08-06 16:56:51 +0300

    finally

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const STORAGE_KEY = 'videoplayer-current-time';
+
+function onPlay(data) {
+  localStorage.setItem(STORAGE_KEY, `${data.seconds}`);
+}
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player.setCurrentTime(Number(localStorage.getItem(STORAGE_KEY)));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+  form: document.querySelector('.feedback-form'),
+};
+
+const STORAGE_KEY = 'feedback-form-state';
+
+let feedbackForm = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+
+const { email, message } = refs.form.elements;
+
+refs.form.addEventListener('submit', onSubmitClickBtn);
+refs.form.addEventListener('input', throttle(onInputSubmit, 500));
+
+function onSubmitClickBtn(evt) {
+  evt.preventDefault();
+
+  if (!email.value || !message.value)
+    return alert('Please fill in all the fields!');
+
+  console.log(feedbackForm);
+
+  evt.target.reset();
+  localStorage.removeItem(STORAGE_KEY);
+  feedbackForm = {};
+}
+
+function onInputSubmit(evt) {
+  feedbackForm[evt.target.name] = evt.target.value.trim();
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(feedbackForm));
+}
+
+function populateTextarea() {
+  if (feedbackForm) {
+    email.value = feedbackForm.email || '';
+    message.value = feedbackForm.message || '';
+  }
+}
+populateTextarea();
